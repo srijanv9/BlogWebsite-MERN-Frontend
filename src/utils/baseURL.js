e023f1ac9dd601370d46b19733f75ce703bce5fb
Kaustav Saha <64833209+kaustavsaha018@users.noreply.github.com>
2022-07-02 01:57:55 +0530

    changed the base url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const baseUrlLocal = "http://localhost:5000";
+const baseUrlLocal = "https://blog-website-mern-backend.herokuapp.com";
 
 const baseUrl = process.env.REACT_APP_API_URL;
-export default baseUrl;
+export default baseUrlLocal;
