48e1c62c765d88c8935fb29ab69bfda0809883b8
Srijan Verma <srijanv9@gmail.com>
2022-12-06 23:16:55 +0530

    final commit

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
-const baseUrlLocal = "https://blog-website-mern-backend.herokuapp.com";
+const baseUrlLocal = "https://blogwebsite-txha.onrender.com";
 
 const baseUrl = process.env.REACT_APP_API_URL;
 export default baseUrlLocal;
